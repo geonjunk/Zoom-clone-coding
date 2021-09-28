7709ac312919ded60757b1628bb7ab85d7475f57
geonjunk <koeunjun7@gmail.com>
2021-09-28 12:35:54 +0900

    chapter1 complete

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,4 +1,46 @@
+const messageList = document.querySelector("ul");
+const nickForm=document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+
 const socket =new WebSocket(`ws://${window.location.host}`);
 //프론트에선 브라우저에 대한 ws 가 implementation이 되어 있음
 //여기서 socket은 서버로의 연결 
 //frontend에서 backend와 socket으로 연결
+
+const makeMessage=(type,payload)=>{
+    const msg={type,payload};
+    return JSON.stringify(msg);
+}
+socket.addEventListener("open",()=>{//connection외 됐을 때
+    console.log("connected to Server");
+});
+
+socket.addEventListener("message",(message)=>{
+  const li = document.createElement("li");
+  li.innerText=message.data;
+  messageList.append(li);
+});
+
+socket.addEventListener("close",()=>{//서버와 연결 끊겼을때
+    console.log("Disconnected from Server");
+});
+
+
+
+messageForm.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    const input= messageForm.querySelector("input");
+    socket.send(makeMessage("new_message",input.value));
+   
+    const li = document.createElement("li");
+    li.innerText=`You : ${input.value}`;//내가 보낸 메시지는 YOu로 
+    messageList.append(li);
+    input.value="";
+});
+
+nickForm.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname",input.value));//text 대신 json형식으로
+    input.value="";
+});
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express=require(`express`);
 const app=express();
 const http=require(`http`);
+const { parse } = require("path");
 const WebSocket=require(`ws`);
 
 app.set("view engine","pug");
@@ -16,9 +17,26 @@ const wss= new WebSocket.Server({server});// 여기서부터 ws 서버
 //둘다 http 서버와 ws서버 같이 돌리수 있음 (2개가 같은 포트에 있긴원하기에 이렇게 하는거 필수 사항은 아님)
 //http서버 위에 ws서버를 만들 수 있게한거임, 2개의 프로토콜이 다 같은 port를 공유
 
+const sockets=[];//연결된 브라우저 구별하기위해
+
 wss.on("connection",(socket)=>{
-    console.log(socket);
+    sockets.push(socket);
+    socket["nickname"]="anono";
+    console.log("connected to Browser")
+    socket.on("close",()=> console.log("Disconnected from the Browser"));//브라우저 껐을 때 이벤트 발생
+    socket.on("message",(msg)=>{
+        const message=JSON.parse(msg);
+        switch(message.type){
+            case "new_message":
+                sockets.forEach((aSocket)=>aSocket.send(`${socket.nickname}: ${message.payload}`));//한 브라우저에서 메시지 받았을 때 다른 브라우저에도 보냄
+            case "nickname":
+                socket["nickname"]=message.payload;//socket안에 정보를 저장할 수있음
+        }   
+    });
 });//누구와 연결됐을때
 //callback으로 socket받음 -> socket은 backend에 연결된 어떤 사람의 정보를 제공 (어떤 것과 연결된 라인)
-//여기서 socket은 연결된 브라우저
+//여기서 socket은 프론트와 연결된 소켓
+//2개이상의 브라우저와 한서버 연결가능
 server.listen(3000,handleListen);
+
+//2가지 type의 data를 구별해서 보내줘야함(nick , message)
